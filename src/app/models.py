dde0a302099ec495cd74861c4116272592c600b1
root <root@ashe>
2017-07-07 10:07:46 +0000

    add model

--- src/app/models.py
+++ src/app/models.py
@@ -4,8 +4,45 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+#  https://www.processon.com/apps/59197d37e4b0bcc15ca2e55c
+#
+
+
+class Job(models.Model):
+
+    class Meta:
+        db_table = 'jobs'
+
+    name = models.TextField(unique=True)
+    description = models.TextField(blank=True, default='')
+    svn_url = models.TextField()
+    svn_username = models.TextField(blank=True, default='')
+    svn_password = models.TextField(blank=True, default='')
+
+    recipient = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Build(models.Model):
+
+    class Meta:
+        db_table = 'builds'
+
+    number = models.IntegerField()
+    job = models.ForeignKey(Job, related_name="builds")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    result = models.TextField(default="")
+
+    def __str__(self):
+        return "{0}:{1}".format(self.job.name, self.number)
+
+
 class Sprint(models.Model):
-    name = models.CharField(max_length = 100, blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField(blank=True, default='')
     end = models.DateField(unique=True)
 
@@ -21,19 +58,19 @@ class Task(models.Model):
 
     STATUS_CHOICES = (
         (STATUS_TODO, "Not Started"),
-        (STATUS_IN_PROGRESS,"In Progress"),
+        (STATUS_IN_PROGRESS, "In Progress"),
         (STATUS_TESTING, "Testing"),
         (STATUS_DONE, "Done"),
     )
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, default = '')
-    sprint = models.ForeignKey(Sprint, blank=True, null = True)
+    description = models.TextField(blank=True, default='')
+    sprint = models.ForeignKey(Sprint, blank=True, null=True)
     status = models.SmallIntegerField(choices=STATUS_CHOICES, default=STATUS_TODO)
-    order = models.SmallIntegerField(default = 0)
+    order = models.SmallIntegerField(default=0)
     assigned = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     started = models.DateField(blank=True, null=True)
     due = models.DateField(blank=True, null=True)
     completed = models.DateField(blank=True, null=True)
-        
+
     def __str__(self):
         return self.name
--- src/app/urls.py
+++ src/app/urls.py
@@ -2,7 +2,7 @@
 from . import views
 from rest_framework import routers
 router = routers.DefaultRouter()  # DefaultRouter会生成rootview
-router.register(r'sprints', views.SprintViewSet)
-router.register(r'tasks', views.TaskViewSet)
+router.register(r'jobs', views.SprintViewSet)
+router.register(r'builds', views.TaskViewSet)
 router.register(r'users', views.UserViewSet)
 router.register(r'userdefines', views.UserDefineViewSet, 'userdefine')
