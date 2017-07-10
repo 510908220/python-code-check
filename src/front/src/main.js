f91303ae7c43065d582cfc1144946ba0eef75e54
westdoorblowcola <510908220@qq.com>
2017-07-10 16:08:37 +0800

    add front

---
+++ src/front/src/main.js
@@ -0,0 +1,66 @@
+// Import System requirements
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import { sync } from 'vuex-router-sync'
+import routes from './routes'
+import store from './store'
+
+// Import Helpers for filters
+import { domain, count, prettyDate, pluralize } from './filters'
+
+// Import Views - Top level
+import AppView from './components/App.vue'
+
+// Import Install and register helper items
+Vue.filter('count', count)
+Vue.filter('domain', domain)
+Vue.filter('prettyDate', prettyDate)
+Vue.filter('pluralize', pluralize)
+
+Vue.use(VueRouter)
+
+// Routing logic
+var router = new VueRouter({
+  routes: routes,
+  mode: 'history',
+  scrollBehavior: function (to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 }
+  }
+})
+
+// Some middleware to help us ensure the user is authenticated.
+router.beforeEach((to, from, next) => {
+  // window.console.log('Transition', transition)
+  if (to.auth && (to.router.app.$store.state.token === 'null')) {
+    window.console.log('Not authenticated')
+    next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  } else {
+    next()
+  }
+})
+
+sync(store, router)
+
+// Start out app!
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#root',
+  router: router,
+  store: store,
+  render: h => h(AppView)
+})
+
+// Check local storage to handle refreshes
+if (window.localStorage) {
+  var localUserString = window.localStorage.getItem('user') || 'null'
+  var localUser = JSON.parse(localUserString)
+
+  if (localUser && store.state.user !== localUser) {
+    store.commit('SET_USER', localUser)
+    store.commit('SET_TOKEN', window.localStorage.getItem('token'))
+  }
+}
