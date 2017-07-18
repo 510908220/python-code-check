6b48854f695f578b30f7b87e812e9fceefa439c5
root <root@ashe>
2017-07-18 16:24:09 +0000

    npm build static避免与django static重复

--- src/pylinter/settings.py
+++ src/pylinter/settings.py
@@ -119,7 +119,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_django/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 DEBUG = eval(os.environ['DEBUG'])
